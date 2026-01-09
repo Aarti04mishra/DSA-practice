d333e08995ae08f5a0f5452e4c17556bd3506286
Aarti Mishra <aarti04mishra@gmail.com>
2026-01-09 23:45:42 +0530

    Striver Sheet

---
+++ src/BinarySearch/KthMissing.java
@@ -0,0 +1,27 @@
+package BinarySearch;
+
+public class KthMissing {
+    public static void main(String[] args) {
+        int[] arr={2,3,4,7,11};
+        int k=5;
+
+        System.out.println(missing(arr,k));
+    }
+
+    static int missing(int[] arr,int k){
+        int low=0;
+        int high=arr.length-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            int missing=arr[mid]-(mid+1);
+            if(missing<k){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+
+        return low+k;
+    }
+}
